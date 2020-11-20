e120721dd4e327ad9d2006cfbb63245bb90d2116
duyangfan <599588568@qq.com>
2020-11-20 16:56:26 +0800

    add info

--- src/js/base.js
+++ src/js/base.js
@@ -71,3 +71,118 @@ new Vue({
         }
     }
 });
+new Vue({
+    el:"#ed",
+    data:{
+
+    },
+    methods:{
+        fun1(){
+           console.log("fun1")
+        },
+        fun2(){
+            console.log("fun2")
+        },
+    }
+});
+new Vue({
+    el:"#for",
+    data:{
+        friut:["B","A","L"]
+    }
+
+});
+new Vue({
+    el:"#entrykey",
+    methods:{
+        func(){
+            console.log(event.code+"  "+event.keyCode+"    "+event.key)
+        }
+    }
+
+});
+new Vue({
+    el:"#class_bond",
+    data:{
+        classStr:"cc"
+    }
+
+
+});
+let com=Vue.component("login",{
+    template:'<h1>template{{msg}}</h1>',
+    data(){
+      return {
+          msg:"数据data"
+      }
+    },
+    methods:{
+        fun(){
+            console.log("test")
+        }
+    }
+})
+new Vue({
+    el:"#com",
+    components: {
+            'login':com
+        }
+    }
+);
+
+let parent=Vue.component('parent',{
+    template:`<div>
+                <p>这是{{msg}}</p>
+                <p>这是数据son:{{sonMsg}}</p>
+                <son @getMsg="fun" :send="info"></son>
+               </div>`,
+    data(){
+        return{
+            msg:'parent',
+            info:'传递数据到son',
+            sonMsg:''
+        }
+    },
+    methods:{
+        fun(m){
+            console.log("父组件函数激活"+m);
+            this.sonMsg=m;
+        }
+    }
+});
+let son=Vue.component('son',{
+    template:`  <div>
+                    <p>这是{{msg}}</p>
+                    <p>parent:{{send}}</p>
+                    <button @click="sendMsg()">发送数据到parent</button>
+                </div>`,
+    props:['send'],
+    data(){
+        return{
+            msg:'son'
+        }
+    },
+    methods:{
+        sendMsg(){
+            //调用this.$emit
+            //参数2，传值
+            console.log("触发父组件自定义事件")
+            this.$emit('getMsg',this.msg)
+        }
+    }
+
+});
+new Vue({
+    el:'#ps',
+    components:{
+        parent,
+        son
+    }
+});
+new Vue({
+    el:'#ps_msg',
+    components:{
+        parent,
+        son
+    }
+});
